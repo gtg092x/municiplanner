86c246c58cbc133f0b1d6d1172782bc4476ef0d3
mdrake <matthew.drake@22squared.com>
2013-11-16 11:17:06 -0500

    Viewchange

--- provider/act.js
+++ provider/act.js
@@ -5,7 +5,7 @@ var acts =
 	 	{
 	 		name:"Soup Kitchen",
 	 		_id:base.genId(),
-	 		media:{src:"/image/foo.jpg"},
+	 		media:{src:"http://clatl.com/imager/imagine-transit-here-the-path-of-the-beltline-seen-here-crossing-ponce-de/b/original/1281751/47b6/news_Feature1-1_12.jpg"},
 	 		distance:10,
 	 		when:new Date(),
 	 		geo: [33.745487,-84.368434],
@@ -14,13 +14,21 @@ var acts =
 	 	{
 	 		name:"City Council Meeting",
 	 		_id:base.genId(),
-	 		media:{src:"/image/foo.jpg"},
+	 		media:{src:"http://clatl.com/imager/imagine-transit-here-the-path-of-the-beltline-seen-here-crossing-ponce-de/b/original/1281751/47b6/news_Feature1-1_12.jpg"},
 	 		distance:10,
 	 		when:new Date(),
 	 		geo: [33.745487,-84.368434],
 	 		tags:['government','council']
 	 	}
 	];
+_.each(acts,function(act){
+	_.extend(act,{
+		whenFormatted:function(){
+			return "now";
+		}
+	});
+});
+
 
 exports.all = function(fn){
 	return fn(null,acts);
