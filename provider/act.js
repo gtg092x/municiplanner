16f0041cdf68b8198e222b9c9594c656f5901cb5
Carson Britt <carson28@gmail.com>
2013-11-16 15:05:15 -0500

    date formatting

--- provider/act.js
+++ provider/act.js
@@ -10,7 +10,13 @@ var acts = function(fn){
 			act.when=new Date();
 			_.extend(act,{
 				whenFormatted:function(){
-					return "now";
+					var month=new Array();
+					month[0]="Jan";	month[1]="Feb";	month[2]="Mar";
+					month[3]="Apr";	month[4]="May"; month[5]="Jun";
+					month[6]="Jul";	month[7]="Aug";	month[8]="Sep";
+					month[9]="Oct";	month[10]="Nov";month[11]="Dec";
+					var d = new Date();
+					return month[d.getMonth()] + " " + d.getDay() + " "+ d.getFullYear();
 				}
 			});
 		});
