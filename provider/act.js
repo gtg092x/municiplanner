908765c4a7fe9fb05a9b7de0eed0298ef06b7b6a
mdrake <matthew.drake@22squared.com>
2013-11-16 12:43:51 -0500

    json stuff

--- provider/act.js
+++ provider/act.js
@@ -1,49 +1,40 @@
 var base = require('./_base');
 var _ = require('underscore')._;
-var acts = 
-	[
-	 	{
-	 		name:"Soup Kitchen",
-	 		_id:base.genId(),
-	 		media:{src:"http://clatl.com/imager/imagine-transit-here-the-path-of-the-beltline-seen-here-crossing-ponce-de/b/original/1281751/47b6/news_Feature1-1_12.jpg"},
-	 		distance:10,
-	 		when:new Date(),
-	 		where:{
-	 			geo: [33.745487,-84.368434],
-	 			address:"101 Yeah Boy Drive"
-	 		},
-	 		tags:['volunteering','charity']
-	 	},
-	 	{
-	 		name:"City Council Meeting",
-	 		_id:base.genId(),
-	 		media:{src:"http://clatl.com/imager/imagine-transit-here-the-path-of-the-beltline-seen-here-crossing-ponce-de/b/original/1281751/47b6/news_Feature1-1_12.jpg"},
-	 		distance:10,
-	 		when:new Date(),
-	 		where:{
-	 			geo: [33.745487,-84.368434],
-	 			address:"101 Yeah Boy Drive"
-	 		},
-	 		tags:['government','council']
-	 	}
-	];
-_.each(acts,function(act){
-	_.extend(act,{
-		whenFormatted:function(){
-			return "now";
-		}
+var fs = require('fs');
+var acts = function(fn){
+	fs.readFile(__dirname+'/../data/activities.json','utf8',function(err,data){
+		data = JSON.parse(data);
+		if(data)
+		_.each(data.acts,function(act){
+			act._id=base.genId();
+			act.when=new Date();
+			_.extend(act,{
+				whenFormatted:function(){
+					return "now";
+				}
+			});
+		});
+		fn(err,data?data.acts:null);
 	});
-});
+};
+
+
 
 
 exports.all = function(fn){
-	return fn(null,acts);
+	acts(function(err,data){
+		fn(err,data);
+	});
 }
 
 exports.find = function(query,fn){
-	return fn(null,acts[0]);
+	acts(function(err,data){
+		fn(err,data[0]);
+	});
 }
 
 exports.where = function(query,fn){
-	return fn(null,acts);
+	acts(function(err,data){
+		fn(err,data);
+	});
 }
